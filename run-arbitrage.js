cf94e40a45e3710bf60cfb12914bf800018d90e6
Stalin Macias <mags961224hjccmt00@gmail.com>
2022-10-24 00:01:44 -0500

    Created logic to determine if there is an arbitrage opportunity or not based on the current prices

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -46,10 +46,10 @@ const init = async () => {
     .on('data', async block => {
       console.log(`New block received. Block # ${block.number}`);
 
-      RECENT_ETH_PRICE = await common.retrieveLatestEthPrice(); // Pull the latest eth price using the Binance API
+      RECENT_ETH_PRICE = Math.round(await common.retrieveLatestEthPrice()); // Pull the latest eth price using the Binance API
       AMOUNT_DAI_WEI = web3.utils.toWei((AMOUNT_ETH * RECENT_ETH_PRICE).toString()) // Calculate the required amount of DAI based on the latest price of ETH
       console.log(`Current ETH Price pulled from the Binance API: ${RECENT_ETH_PRICE}`);
-      console.log(`AMOUNT_DAI_WEI based on ETH Price from the Binance API: ${web3.utils.fromWei(AMOUNT_DAI_WEI)}`);
+      console.log(`AMOUNT_DAI based on the ETH Price from the Binance API: ${web3.utils.fromWei(AMOUNT_DAI_WEI)}`);
 
       /*
       * Querying the Kyber markets to get the token's prices
@@ -94,6 +94,36 @@ const init = async () => {
       };
       console.log("\n ETH/DAI price on Uniswap");
       console.log(uniswapRates);
+
+      const gasPrice = await web3.eth.getGasPrice();
+      //200000 is picked arbitrarily, will be replaced by the actual tx gas cost using Web3 estimateGas()
+      const txCost = 200000 * parseInt(gasPrice);
+      const uniswapETHAveragePrice = (uniswapRates.buy + uniswapRates.sell) / 2;
+      const kyberETHAveragePrice = (kyberRates.buy + kyberRates.sell) / 2;
+      // Calculate the current ETH price by getting the average of the 3 prices that were pulled from Uniswap, Kyber and the Binance API
+      const currentEthPrice = ( uniswapETHAveragePrice + kyberETHAveragePrice + RECENT_ETH_PRICE ) / 3;
+
+      // arbitraging by buying in Kyber and selling in Uniswap
+      const profit1 = (parseInt(AMOUNT_ETH_WEI) / 10 ** 18) * (uniswapRates.sell - kyberRates.buy) - (txCost / 10 ** 18) * currentEthPrice;
+      
+      // arbitraging by buying in Uniswap and selling in Kyber
+      const profit2 = (parseInt(AMOUNT_ETH_WEI) / 10 ** 18) * (kyberRates.sell - uniswapRates.buy) - (txCost / 10 ** 18) * currentEthPrice;
+
+      if(profit1 > 0) {
+        console.log('Arbitrage opportunity found!');
+        console.log(`Buy ETH on Kyber at ${kyberRates.buy} dai`);
+        console.log(`Sell ETH on Uniswap at ${uniswapRates.sell} dai`);
+        console.log(`Expected profit: ${profit1} dai`);
+        //Execute arb Kyber <=> Uniswap
+      } else if(profit2 > 0) {
+        console.log('Arbitrage opportunity found!');
+        console.log(`Buy ETH from Uniswap at ${uniswapRates.buy} dai`);
+        console.log(`Sell ETH from Kyber at ${kyberRates.sell} dai`);
+        console.log(`Expected profit: ${profit2} dai`);
+        //Execute arb Uniswap <=> Kyber
+      } else {
+        console.log("At the moment there is no arbitrage opportunity");
+      }
 
       console.log("\n\n");
 
